3d0611b7601377af31a18539e6ec0ca9ad9e991c
Bramar95 <bramar2195@gmail.com>
2021-12-07 22:32:17 -0300

    agregué modificaciones al archivo detailProduct.html y creé productDetail.css

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-const express =require("express")
-const app = express()
-const path = require("path")
+const express =require("express");
+const app = express();
+const path = require("path");
 
 app.listen(3000,()=> console.log ("Estamos usando el puerto 3000"));
 
-const staticFiles = express.static(path.join(__dirname,"/public"))
-app.use(staticFiles)
+const staticFiles = express.static(path.join(__dirname,"/public"));
+app.use(staticFiles);
 
 app.get("/", (req,res)=> {
-    const pathHtml = path.resolve(__dirname,"views/home.html")
-    res.sendFile(pathHtml)
-})
+    const pathHtml = path.resolve(__dirname,"views/home.html");
+    res.sendFile(pathHtml);
+});
+
 app.get("/login", (req,res)=> {
-    const pathHtml = path.resolve(__dirname,"views/login.html")
-    res.sendFile(pathHtml)
-})
+    const pathHtml = path.resolve(__dirname,"views/login.html");
+    res.sendFile(pathHtml);
+});
+
+app.get("/product-detail", (req,res)=> {
+    const pathProductDetail = path.resolve(__dirname, "views/productDetail.html");
+    res.sendFile(pathProductDetail);
+});
