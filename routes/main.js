750a90f195047bd1e878ee27b57e07b2d7cd670b
JoseMiranda1 <josemiranda.rp@hotmail.com>
2022-01-02 23:54:59 -0300

    login-register responsive, agrego usuario profile y edito nav bar del header

--- routes/main.js
+++ routes/main.js
@@ -6,5 +6,6 @@ router.get("/", controllers.home);
 router.get("/productCart", controllers.productCart);
 router.get("/product-detail", controllers.productDetail)
 router.get("/productEdit", controllers.productEdit)
-
+router.get("/user/profile",controllers.profile);
+router.post("/user/profile",controllers.profile)
 module.exports= router;
