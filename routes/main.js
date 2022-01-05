a3900c6193cc433971b0e66918e1086261c9b758
Mati1206 <mattiasaragon@gmail.com>
2022-01-05 18:18:01 -0300

    divido productos de edit, responsive de head, comento hover de imagenes en el Home

--- routes/main.js
+++ routes/main.js
@@ -7,5 +7,6 @@ router.get("/productCart", controllers.productCart);
 router.get("/productDetail", controllers.productDetail)
 router.get("/productEdit", controllers.productEdit)
 router.get("/user/profile",controllers.profile);
-router.post("/user/profile",controllers.profile)
+router.post("/user/profile",controllers.profile);
+router.get("/productCreate", controllers.productCreate);
 module.exports= router;
