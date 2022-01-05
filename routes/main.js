21c5a1e0f7e9954cb5d616bfedbf0b201e74ac9c
Bramar95 <bramar2195@gmail.com>
2022-01-04 22:31:39 -0300

    agrego estilos a productEdit

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ const controllers=require("../controllers/main");
 
 router.get("/", controllers.home);
 router.get("/productCart", controllers.productCart);
-router.get("/product-detail", controllers.productDetail)
+router.get("/productDetail", controllers.productDetail)
 router.get("/productEdit", controllers.productEdit)
 router.get("/user/profile",controllers.profile);
 router.post("/user/profile",controllers.profile)
