60b48b9d58cf5e140928f6d43ca60eed5c06eddc
Mati1206 <mattiasaragon@gmail.com>
2022-02-16 20:35:13 -0300

    Correccion de /products

--- controllers/products.js
+++ controllers/products.js
@@ -2,8 +2,6 @@ const fs=require("fs")
 const path=require("path")
 const filePath=path.resolve(__dirname,"../data/products.json")
 const productsArray=JSON.parse(fs.readFileSync(filePath,"utf8"))
-
-
 const controllers={
     products: (req,res)=>{
         res.render("products",{
@@ -17,7 +15,6 @@ const controllers={
         const productId= req.params.id
         res.send("Estamos en producto con id " + productId)
     },
-    
     productEdit:(req,res)=>{
         res.render("productEdit")
     },
@@ -28,24 +25,21 @@ const controllers={
     add:(req,res)=>{          
         const generateID = () => {
             const lastProduct = productsDB[productsDB.length - 1];
-			const lastID = lastProduct.id;
-			return lastID + 1;
+            const lastID = lastProduct.id;
+            return lastID + 1;
         }
-
         productsArray.push({
         id: generateID(),
         nameProduct: req.params.name-product-create,
-        tradeEdit: req.params.trade-create , 
+        tradeEdit: req.params.trade-create ,
         compositionCreate: req.params.composition-create,
         propertyCreate: req.params.property-create,      
         priceCreate: req.params.price-create,
         desctiptionCreate: req.params.description-create,
         })
         fs.writeFileSync(filePath,JSON.stringify(productsArray, null, " "))
-
         res.redirect("/products")
     },
-  
     delete: (req, res) => {
         let idProducto = req.params.id;
         productsArray = productsArray.filter(product => product.id != idProducto);
