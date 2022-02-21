98298b4e7ed39305ee03973664184361cb278e22
RoSeiler <seilerrocio@gmail.com>
2022-02-21 13:40:27 -0300

    primera parte del sprint

---
+++ lasabanadb.sql
@@ -0,0 +1,357 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-02-2022 a las 21:31:30
+-- Versión del servidor: 10.4.13-MariaDB
+-- Versión de PHP: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `lasabanadb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `brands`
+--
+
+CREATE TABLE `brands` (
+  `idBrand` int(10) UNSIGNED NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cartproduct`
+--
+
+CREATE TABLE `cartproduct` (
+  `idCartProduct` int(10) UNSIGNED NOT NULL,
+  `productPrice` decimal(18,2) DEFAULT NULL,
+  `quantity` int(10) UNSIGNED DEFAULT NULL,
+  `idProduct` int(10) UNSIGNED DEFAULT NULL,
+  `idCart` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carts`
+--
+
+CREATE TABLE `carts` (
+  `idCart` int(10) UNSIGNED NOT NULL,
+  `idUser` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
+CREATE TABLE `categories` (
+  `idCategories` int(10) UNSIGNED NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoryproduct`
+--
+
+CREATE TABLE `categoryproduct` (
+  `idCategoryProduct` int(10) UNSIGNED NOT NULL,
+  `idProduct` int(10) UNSIGNED DEFAULT NULL,
+  `idCategories` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `colorproduct`
+--
+
+CREATE TABLE `colorproduct` (
+  `idColorProduct` int(10) UNSIGNED NOT NULL,
+  `idProduct` int(10) UNSIGNED DEFAULT NULL,
+  `idColor` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `colors`
+--
+
+CREATE TABLE `colors` (
+  `idColor` int(10) UNSIGNED NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `idProduct` int(10) UNSIGNED NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `price` decimal(18,2) DEFAULT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  `idBrand` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sizeproduct`
+--
+
+CREATE TABLE `sizeproduct` (
+  `idSizeProduct` int(10) UNSIGNED NOT NULL,
+  `idProduct` int(10) UNSIGNED DEFAULT NULL,
+  `idSize` int(10) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sizes`
+--
+
+CREATE TABLE `sizes` (
+  `idSize` int(10) UNSIGNED NOT NULL,
+  `name` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `idUser` int(10) UNSIGNED NOT NULL,
+  `userName` varchar(150) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `userPassword` varchar(50) NOT NULL,
+  `adress` varchar(50) DEFAULT NULL,
+  `postCode` varchar(25) DEFAULT NULL,
+  `country` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`idBrand`);
+
+--
+-- Indices de la tabla `cartproduct`
+--
+ALTER TABLE `cartproduct`
+  ADD PRIMARY KEY (`idCartProduct`),
+  ADD KEY `idProduct` (`idProduct`),
+  ADD KEY `idCart` (`idCart`);
+
+--
+-- Indices de la tabla `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`idCart`),
+  ADD KEY `idUser` (`idUser`);
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`idCategories`);
+
+--
+-- Indices de la tabla `categoryproduct`
+--
+ALTER TABLE `categoryproduct`
+  ADD PRIMARY KEY (`idCategoryProduct`),
+  ADD KEY `idProduct` (`idProduct`),
+  ADD KEY `idCategories` (`idCategories`);
+
+--
+-- Indices de la tabla `colorproduct`
+--
+ALTER TABLE `colorproduct`
+  ADD PRIMARY KEY (`idColorProduct`),
+  ADD KEY `idProduct` (`idProduct`),
+  ADD KEY `idColor` (`idColor`);
+
+--
+-- Indices de la tabla `colors`
+--
+ALTER TABLE `colors`
+  ADD PRIMARY KEY (`idColor`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`idProduct`),
+  ADD KEY `idBrand` (`idBrand`);
+
+--
+-- Indices de la tabla `sizeproduct`
+--
+ALTER TABLE `sizeproduct`
+  ADD PRIMARY KEY (`idSizeProduct`),
+  ADD KEY `idProduct` (`idProduct`),
+  ADD KEY `idSize` (`idSize`);
+
+--
+-- Indices de la tabla `sizes`
+--
+ALTER TABLE `sizes`
+  ADD PRIMARY KEY (`idSize`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`idUser`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `idBrand` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cartproduct`
+--
+ALTER TABLE `cartproduct`
+  MODIFY `idCartProduct` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `carts`
+--
+ALTER TABLE `carts`
+  MODIFY `idCart` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `idCategories` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoryproduct`
+--
+ALTER TABLE `categoryproduct`
+  MODIFY `idCategoryProduct` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `colorproduct`
+--
+ALTER TABLE `colorproduct`
+  MODIFY `idColorProduct` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `colors`
+--
+ALTER TABLE `colors`
+  MODIFY `idColor` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `idProduct` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sizeproduct`
+--
+ALTER TABLE `sizeproduct`
+  MODIFY `idSizeProduct` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sizes`
+--
+ALTER TABLE `sizes`
+  MODIFY `idSize` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `idUser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cartproduct`
+--
+ALTER TABLE `cartproduct`
+  ADD CONSTRAINT `cartproduct_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `products` (`idProduct`),
+  ADD CONSTRAINT `cartproduct_ibfk_2` FOREIGN KEY (`idCart`) REFERENCES `carts` (`idCart`);
+
+--
+-- Filtros para la tabla `carts`
+--
+ALTER TABLE `carts`
+  ADD CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`);
+
+--
+-- Filtros para la tabla `categoryproduct`
+--
+ALTER TABLE `categoryproduct`
+  ADD CONSTRAINT `categoryproduct_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `products` (`idProduct`),
+  ADD CONSTRAINT `categoryproduct_ibfk_2` FOREIGN KEY (`idCategories`) REFERENCES `categories` (`idCategories`);
+
+--
+-- Filtros para la tabla `colorproduct`
+--
+ALTER TABLE `colorproduct`
+  ADD CONSTRAINT `colorproduct_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `products` (`idProduct`),
+  ADD CONSTRAINT `colorproduct_ibfk_2` FOREIGN KEY (`idColor`) REFERENCES `colors` (`idColor`);
+
+--
+-- Filtros para la tabla `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`idBrand`) REFERENCES `brands` (`idBrand`);
+
+--
+-- Filtros para la tabla `sizeproduct`
+--
+ALTER TABLE `sizeproduct`
+  ADD CONSTRAINT `sizeproduct_ibfk_1` FOREIGN KEY (`idProduct`) REFERENCES `products` (`idProduct`),
+  ADD CONSTRAINT `sizeproduct_ibfk_2` FOREIGN KEY (`idSize`) REFERENCES `sizes` (`idSize`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
