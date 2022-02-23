e07d1bb9c810ae9e8155517f44787a87fc2e970d
RoSeiler <seilerrocio@gmail.com>
2022-02-23 15:50:47 -0300

    probando git

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -3,32 +3,6 @@ const controllers={
     home:(req,res)=>{
        res.render("home");
     },
-<<<<<<< HEAD
-    productCart:(req,res)=> {        
-        res.render("productCart");
-    },
-    productDetail:(req,res)=>{
-        res.render("productDetail")
-    },
-    productEdit:(req,res)=>{
-        res.render("productEdit")
-    },
-    productCreate:(req,res)=>{
-        res.render("productCreate")
-    },
-    profile:(ewq,res)=>{
-        const usuarioPerfil=[{
-            name: "José Miranda",
-            document: 4076484854,    
-            tel: 4152415241,
-            direccion: "calle falsa 123"
-        }]
-        
-        res.render("profile",{user: usuarioPerfil})
-
-    }
-    
-=======
     cambiosDevoluciones:(req,res)=>{
         res.render("cambiosDevoluciones");
     },
@@ -43,6 +17,5 @@ const controllers={
     },    
 
    
->>>>>>> afd22df9dd1d1b18dd45de0a334356fa31232ef6
 }
 module.exports= controllers;
